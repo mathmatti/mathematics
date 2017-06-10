a7fdf5c4355778d66aaa7c3ac2a62fcc7cbc665f
giuseppe2048 <bauo81@gmail.com>
2017-06-10 21:28:10 +0200

    bash

---
+++ code/bash/index.sh
@@ -0,0 +1,3 @@
+#https://www.cyberciti.biz/faq/bash-for-loop/
+#https://unix.stackexchange.com/questions/50487/how-to-move-all-files-and-folders-via-mv-command
+#http://tldp.org/HOWTO/Bash-Prog-Intro-HOWTO-5.html
